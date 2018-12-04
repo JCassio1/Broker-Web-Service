4f20ad262fd1702b8bf2810fd0cd90b00c002c6b
JCassio <JCassio@192.168.169.2>
2018-12-04 14:46:33 +0000

    Correct error when unMarshalling wrong root element (only .xsd root element can be marshalled)

--- NetBeansProjects/Broker_WebService/src/broker_webservice/Broker_WebService.java
+++ NetBeansProjects/Broker_WebService/src/broker_webservice/Broker_WebService.java
@@ -6,13 +6,16 @@
 package broker_webservice;
 
 import BrokerInformation.*;
-import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.GregorianCalendar;
 import java.util.List;
 import javax.xml.bind.JAXB;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 /**
  *
@@ -26,11 +29,19 @@ public class Broker_WebService {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, DatatypeConfigurationException {
         
-        String myCompanyName = "";
+        //Convert date to XML date format
+        GregorianCalendar theDate = new GregorianCalendar();
+        theDate.set(GregorianCalendar.YEAR, 2018);
+        theDate.set(GregorianCalendar.MONTH, 11);
+        theDate.set(GregorianCalendar.DATE, 22);
+        XMLGregorianCalendar xmlDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(theDate);
         
+        //call objects
         SharesToday shares = new SharesToday();
+        SharePrice shareDetails = new SharePrice();
+        
         List<Broker> shares_today =  shares.getBrokerCollection();
         
         
@@ -40,27 +51,44 @@ public class Broker_WebService {
         brokers.setCompanyName("Joselson .INC");
         brokers.setCompanySymbol("thisSymbol");
         brokers.setAvailableShares(300);
+        //------
+        shareDetails.setCurrency("GBP");
+        shareDetails.setValue(340);
+        shareDetails.setLastUpdate(xmlDate);
+        brokers.setSharePrice(shareDetails);
+        
         shares_today.add(brokers);
         
         
-        //Convenient way to marshall objects
-        StringWriter sw = new StringWriter();
-        JAXB.marshal(brokers, sw);
-        String xmlString = sw.toString();
+//        //Convenient way to marshall objects
+//        StringWriter sw = new StringWriter();
+//        JAXB.marshal(brokers, sw);
+//        String xmlString = sw.toString();
+
+        try {            
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(shares.getClass().getPackage().getName());
+            javax.xml.bind.Marshaller marshaller = jaxbCtx.createMarshaller();
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"); //NOI18N
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.marshal(shares, System.out);
+        } catch (javax.xml.bind.JAXBException ex) {
+            // XXXTODO Handle exception
+            java.util.logging.Logger.getLogger("global").log(java.util.logging.Level.SEVERE, null, ex); //NOI18N
+        }
         
-        //Writing data to file
-        FileOutputStream outputStream = new FileOutputStream("shares_today.txt");
-        byte[] strToBytes = xmlString.getBytes();
-        outputStream.write(strToBytes);
-        outputStream.close();
-        
-        //Delete me afterwards
-        System.out.println(xmlString);
-        
-        //Convenient way to unMarshall
-        Broker customer = JAXB.unmarshal(new StringReader(xmlString), Broker.class);
-        
-        System.out.println(customer);
+//        //Writing data to file
+//        FileOutputStream outputStream = new FileOutputStream("shares_today.txt");
+//        byte[] strToBytes = xmlString.getBytes();
+//        outputStream.write(strToBytes);
+//        outputStream.close();
+//        
+//        //Delete me afterwards
+//        System.out.println(xmlString);
+//        
+//        //Convenient way to unMarshall
+//        Broker customer = JAXB.unmarshal(new StringReader(xmlString), Broker.class);
+//        
+//        System.out.println(customer);
         
         
         
